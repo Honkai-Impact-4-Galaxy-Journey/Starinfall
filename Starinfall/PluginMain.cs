b092e68256e0d90daa68f4ab8860bec1f67eda16
Griseo-bh3rd <atmlz32@163.com>
2024-11-28 17:53:12 +0800

    添加项目文件。

---
+++ Starinfall/PluginMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginAPI.Core.Attributes;
+
+namespace Starinfall
+{
+    public class PluginConfig
+    {
+        
+    }
+    public class PluginMain
+    {
+        [PluginEntryPoint("Starinfall","1.0.0","-","Honkai Impact 4: Galaxy Journey Team")]
+        public void OnEnabled()
+        {
+
+        }
+    }
+}
