77e680bfa9ef582141c9e061bc3c8ff7998f94bd
Sirin3rd <1532599172@qq.com>
2025-02-11 11:23:32 +0800

    depend: 换用Labapi

--- Starinfall/PluginMain.cs
+++ Starinfall/PluginMain.cs
@@ -1,22 +1,35 @@
-﻿using System;
+﻿using LabApi.Features.Console;
+using LabApi.Loader.Features.Plugins;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PluginAPI.Core.Attributes;
 
 namespace Starinfall
 {
-    public class PluginConfig
+    public class MainConfig
     {
-        
+        [Description("MySQL连接字符串")]
+        public string ConnectionString { get; set; } = "server=127.0.0.1;database=scp;user=root;password=123456;charset=utf-8";
     }
-    public class PluginMain
+    public class PluginMain : Plugin<MainConfig>
     {
-        [PluginEntryPoint("Starinfall","1.0.0","-","Honkai Impact 4: Galaxy Journey Team")]
-        public void OnEnabled()
+        public static PluginMain Instance { get; private set; }
+        public override string Author => "崩坏4:银河漫游团队";
+        public override string Name => "Starinfall";
+        public override Version Version => new Version(0,1,0,0);
+        public override string Description => "服务器核心插件";
+        public override Version RequiredApiVersion => new Version("0.0.0.0");
+        public override void Enable()
         {
-
+            Instance = this;
+            Logger.Info("Plugin Loaded");
+        }
+        public override void Disable()
+        {
+            Logger.Info("Plugin Disabled");
         }
     }
 }
